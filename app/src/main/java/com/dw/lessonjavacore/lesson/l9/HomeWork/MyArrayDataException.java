79f04677f68a5dba88898b157b93710d020de65e
77den <77denisov@gmail.com>
2021-11-23 17:57:46 +0300

    Homework 9

---
+++ app/src/main/java/com/dw/lessonjavacore/lesson/l9/HomeWork/MyArrayDataException.java
@@ -0,0 +1,10 @@
+package com.dw.lessonjavacore.lesson.l9.HomeWork;
+
+public class MyArrayDataException extends RuntimeException{
+    public MyArrayDataException() {
+    }
+
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
